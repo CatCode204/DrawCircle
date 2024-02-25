417708d755b404c0e1bf30efb241e36dbdfc79a5
Mèo Bách Khoa <truongtaman2004@gmail.com>
2024-02-26 06:01:08 +0700

    Edit vector.cpp vector.hpp

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -8,25 +8,26 @@ public:
     virtual Vector operator + (const Vector&) const = 0;
     virtual Vector operator - (const Vector&) const = 0;
     virtual Vector operator * (const float&) const = 0;
-    virtual Vector& operator*=(const float&);
-    virtual Vector& operator+=(const Vector&);
-    virtual Vector& operator-=(const Vector&);
+    virtual Vector& operator*=(const float&) = 0;
+    virtual Vector& operator+=(const Vector&) = 0;
+    virtual Vector& operator-=(const Vector&) = 0;
     virtual float DotProduct (const Vector&) const = 0;
 };
 
-class Vector2: public Point2,public Vector2 {
+class Vector2: public Vector, public Point2 {
 public:
     float Magnitude() const;
     Vector2 operator + (const Vector2&) const;
     Vector2 operator - (const Vector2&) const;
-    Vector2 operator * (const float&) const;
+    Vector2 operator * (const Vector2&) const;
     float DotProduct(const Vector2&) const;
     Vector2& operator*=(const float&);
     Vector2& operator+=(const Vector2&);
     Vector2& operator-=(const Vector2&);
+    float DotProduct(const Vector&) const;
 };
 
-class Vector3 : public Vector, Point3 {
+class Vector3 : public Vector, public Point3 {
 public:
     float Magnitude() const;
     Vector3 operator + (const Vector3&) const;
--- lib/Vector.cpp
+++ lib/Vector.cpp
@@ -1,2 +1,6 @@
 #include <Vector.hpp>
+#include <cmath>
 
+float Vector2::Magnitude() const {
+    return sqrt(this->_x * this->_x + this->_y * this->_y);
+}
